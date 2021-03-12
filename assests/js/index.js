c5f18554a4c6e692009eca75854881340be8f047
Khadijaislambd <contact.khadijabegum@gmail.com>
2021-03-12 20:31:58 +0600

    first js

---
+++ assests/js/index.js
@@ -0,0 +1,36 @@
+var mark = 30;
+
+if (mark >= 80) {
+    console.log ("A+")
+}
+else if ((mark >= 75) && (mark <=80)) {
+    console.log ("A")
+}
+else if ((mark >= 70) && (mark <=75)) {
+    console.log ("A-")
+}
+else if ((mark >= 65) && (mark <=70)) {
+    console.log ("B+")
+}
+else if ((mark >= 60) && (mark <=65)) {
+    console.log ("B")
+}
+
+else if ((mark >= 55) && (mark <=60)) {
+    console.log ("B-")
+}
+
+else if ((mark >= 50) && (mark <=55)) {
+    console.log ("C+")
+}
+
+else if ((mark >= 45) && (mark <=50)) {
+    console.log ("C")
+}
+
+else if ((mark >= 40) && (mark <=45)) {
+    console.log ("D")
+}
+else   {
+    console.log ("F")
+}
